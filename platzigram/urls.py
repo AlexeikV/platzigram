ba36e7d9fd15795da62b0103b5ca212f86eb4b9b
AlexeikV <evaldezs1200@alumno.ipn.mx>
2020-11-19 20:26:16 -0600

    Realizacion de uso de funcion Get en DJango

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from platzigram import views
 
 urlpatterns = [
-    path('hello-world', views.hello_world)
+    path('hello-world/', views.hello_world),
+    path('sorted/', views.sort_integers),
+    path('hi/<str:name>/<int:age>', views.say_hi),
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 """Utilidades"""
 from datetime import datetime
+import json
 
 def hello_world(request):
     """Funcion hello_world retorna una http"""
@@ -11,3 +12,25 @@ def hello_world(request):
     return HttpResponse('Oh hi!, the current time is {now}'.format
                         (now=now)
                         )
+
+def sort_integers(request):
+    """Return a JSON response with sorted integers."""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': 'Integers sorted successfully.'
+    }
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+    )
+
+def say_hi(request, name, age):
+    """Return a greeting."""
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}: Welcome to Platzigram'.format(name)
+    return HttpResponse(message)
